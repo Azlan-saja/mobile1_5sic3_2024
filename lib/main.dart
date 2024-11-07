1fd88ea0751a084d8593160b008d8ccd39798233
Azlan-saja <azlansaja19@gmail.com>
2024-11-07 20:14:23 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController nilaiPanjang = TextEditingController();
+    TextEditingController nilaiLebar = TextEditingController();
+    TextEditingController hasilLuas = TextEditingController();
+
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
@@ -17,9 +21,85 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Aplikasi 5SCI3'),
+          title: const Text(
+            'Aplikasi 5SCI3',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.purple,
         ),
-        body: const Text('Ini adalah Body'),
+        body: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            children: [
+              const Text(
+                'HITUNG PERSEGI PANJANG',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              TextFormField(
+                keyboardType: const TextInputType.numberWithOptions(),
+                controller: nilaiPanjang,
+                decoration: const InputDecoration(
+                  labelText: 'Nilai Panjang',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+              TextFormField(
+                keyboardType: const TextInputType.numberWithOptions(),
+                controller: nilaiLebar,
+                decoration: const InputDecoration(
+                  labelText: 'Nilai Lebar',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  double xhasil = double.parse(nilaiPanjang.text) *
+                      double.parse(nilaiLebar.text);
+                  hasilLuas.text = xhasil.toString();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text(
+                  'Luas',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              TextFormField(
+                controller: hasilLuas,
+                decoration: const InputDecoration(
+                  labelText: 'Luas',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'User',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.exit_to_app),
+              label: 'Keluar',
+            ),
+          ],
+        ),
       ),
     );
   }
